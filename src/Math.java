77ea07724010f00de7d82972b104722156fd4b91
Aroshana Dissanayake <aroshanaprabhathqualifications@gmail.com>
2024-08-17 18:24:15 +0530

    calculations

--- src/Math.java
+++ src/Math.java
@@ -5,6 +5,66 @@ public class Math {
         int x = 10;
         int y = 5;
 
+        int answers = x + y;
+        System.out.println(answers);
+
+        //&& ||
+
+        int a = 35;
+        int b = 71;
+
+
+        if( (a >= 50 && b<= 70) || (a >= 40 && b<= 80) ){
+            System.out.println("succes");
+        }else{
+            System.out.println("error");
+        }
+
+
+
+        //Switch case Statement
+
+
+        int weekday = 2;
+
+        switch(weekday){
+            case 1:
+                System.out.println("today is monday");
+                break;
+            case 2:
+                System.out.println("today is a tuesday");
+        }
+
+        //lopps
+
+
+        //aray
+
+
+
+        int[] numbers = {1,2,3,4,5,6};
+
+        System.out.println(numbers[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
