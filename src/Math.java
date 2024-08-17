c782d29729434b2f1844f2140998f8fe50e9ac69
Aroshana Dissanayake <aroshanaprabhathqualifications@gmail.com>
2024-08-17 11:58:39 +0530

    second commit

---
+++ src/Math.java
@@ -0,0 +1,12 @@
+public class Math {
+
+    public static void main(String[]args){
+
+        int x = 10;
+        int y = 5;
+
+
+
+
+    }
+}
--- src/model/A.java
+++ src/model/A.java
@@ -1,9 +1,17 @@
 package model;
 
+import java.util.Scanner;
+
 public class A {
 
     public static void main(String[]args){
 
+
+        Scanner x = new Scanner(System.in);
+
+        String name = x.nextLine();
+
+        System.out.println("kamal");
 
 
 
